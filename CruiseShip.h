f2aeda207d513cf0a5d7d8daf1f1f11e6f850a84
NINA <ninatsao@ningningning.local>
2025-07-24 14:13:47 -0700

    3rd commit

--- CruiseShip.h
+++ CruiseShip.h
@@ -1,6 +1,17 @@
 #ifndef CRUISESHIP_H
 #define CRUISESHIP_H
+#include "Ship.h"
+#include <string>
 
-
+class CruiseShip : public Ship
+{
+public:
+    CruiseShip(const std::string& name, const std::string& year, int passenger);
+    void setMaxPassenger(int passenger);
+    int getMaxPassenger() const;
+    void print() const override;
+private:
+    int maxPassenger;
+};
 
 #endif
